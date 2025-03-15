27b8fc1c1c60609ff35f76deffbce787dd528e45
Lance Belen <lancebelen.a@gmail.com>
2025-03-15 20:01:07 +1100

    Task 1

---
+++ task1.py
@@ -0,0 +1,24 @@
+def extract_information(property_string: str) -> dict:
+    raise NotImplementedError
+
+def add_feature(property_dict: dict, feature: str) -> None:
+    raise NotImplementedError
+
+def remove_feature(property_dict: dict, feature: str) -> None:
+    raise NotImplementedError
+
+def main():
+    sample_string = "P10001,3 Antrim Place Langwarrin VIC 3910,4,2,2,-38.16655678,145.1838435,,608,257,870000,dishwasher;central heating"
+    property_dict = extract_information(sample_string)
+    print(f"The first property is at {property_dict['full_address']} and is valued at ${property_dict['price']}")
+
+    sample_string_2 = "P10002,G01/7 Rugby Road Hughesdale VIC 3166,2,1,1,-37.89342337,145.0862616,1,,125,645000,dishwasher;air conditioning;balcony"
+    property_dict_2 = extract_information(sample_string_2)
+
+    print(f"The second property is in {property_dict_2['suburb']} and is located on floor {property_dict_2['floor_number']}")
+
+    add_feature(property_dict, 'electric hot water')
+    print(f"Property {property_dict['prop_id']} has the following features: {property_dict['property_features']}")
+
+if __name__ == '__main__':
+    main()
