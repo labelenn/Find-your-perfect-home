621e2d844b19da5dfce97eca8a89b3318cbd8e80
Lance Belen <lancebelen.a@gmail.com>
2025-03-15 20:15:07 +1100

    docstring for task 1 extract_information function

--- task1.py
+++ task1.py
@@ -1,4 +1,25 @@
 def extract_information(property_string: str) -> dict:
+    '''
+    Extracts the property's information from the given string, and returns a dictionary containing the information.
+    The dictionary will contain the following:
+    - prop_id <str>: the property's ID
+    - prop_type <str>: the type of the property (either 'house' or 'apartment')
+    - full_address <str>: this is the address of the property
+      -- if it's a house, it will be in the format of "<street number> <street name> <street type> <suburb> <state code> <postcode>"
+      -- if it's an apartment, it will be in the format of "<apartment number>/<street number> <street name> <street type> <suburb> <state code> <postcode>"
+    - suburb <str>: the suburb of the property
+    - bedrooms <int>: the number of bedrooms in the property
+    - bathrooms <int>: the number of bathrooms in the property
+    - parking_spaces <int>: the number of car spaces in the property
+    - latitude <float>: the latitude of the property
+    - longitude <float>: the longitude of the property
+    - floor_number <int>: the floor number of the property (ONLY if it's an apartment. This will be excluded if it's a house)
+    - land_area <int>: land area in m^2 (ONLY if it's a house. This will be excluded if it's an apartment)
+    - floor_area <int>: floor area in m^2 of the property
+    - price <int>: the predicted price of the property
+    - property_features <list of strings>: a semi-color separated list of the features of the property. Could include solar, air conditioning, dishwasher, floorboards, central heating, etc.
+      -- this can have 0 items, and if it does, make an empty list
+    '''
     raise NotImplementedError
 
 def add_feature(property_dict: dict, feature: str) -> None:
