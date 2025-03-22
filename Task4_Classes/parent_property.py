5b80eea481154f2922abcd68f128ef621ed2561a
Lance Belen <lancebelen.a@gmail.com>
2025-03-22 19:39:20 +1100

    Abstract Parent class implemented

--- Task4_Classes/parent_property.py
+++ Task4_Classes/parent_property.py
@@ -11,70 +11,157 @@ class Property(ABC):
                         price: int,
                         property_features: List[str],
                         coordinates: Tuple[float, float]):
-        raise NotImplementedError
+        
+        '''
+        Initializes the Property object with the following attributes:
+        - prop_id <str>: the property's ID
+        - bedrooms <int>: the number of bedrooms in the property
+        - bathrooms <int>: the number of bathrooms in the property
+        - parking_spaces <int>: the number of car spaces in the property
+        - full_address <str>: this is the address of the property
+          -- if it's a house, it will be in the format of "<street number> <street name> <street type> <suburb> <state code> <postcode>"
+          -- if it's an apartment, it will be in the format of "<apartment number>/<street number> <street name> <street type> <suburb> <state code> <postcode>"
+        - floor_area <int>: floor area in m^2 of the property
+        - price <int>: the predicted price of the property
+        - property_features <list of strings>: a semi-colon separated list of the features of the property. Could include solar, air conditioning, dishwasher, floorboards, central heating, etc.
+          -- this can have 0 items, and if it does, make an empty list
+        - coordinates <tuple of floats>: the latitude and longitude of the property
+        '''
+        self.prop_id = prop_id
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        self.parking_spaces = parking_spaces
+        self.full_address = full_address
+        self.floor_area = floor_area
+        self.price = price
+        self.property_features = property_features
+        self.coordinates = coordinates
     
     def get_prop_id(self) -> str:
-        raise NotImplementedError
+        ''''
+        Returns the property ID
+        '''
+        return self.prop_id
 
     def get_full_address(self) -> str:
-        raise NotImplementedError
+        ''''
+        Returns the full address of the property
+        '''
+        return self.full_address
 
     def get_suburb(self) -> str:
-        raise NotImplementedError
+        '''
+        Returns the suburb of the property
+        '''
+        return self.full_address.split(' ')[-3]
     
     def get_prop_type(self) -> str:
-        raise NotImplementedError
+        '''
+        Returns the type of the property
+        '''
+        return 'apartment' if '/' in self.full_address.split(' ')[0] else 'house'
     
     def set_bedrooms(self, bedrooms: int) -> None:
-        raise NotImplementedError
+        '''
+        Sets the number of bedrooms in the property
+        '''
+        self.bedrooms = bedrooms
     
     def get_bedrooms(self) -> int:
-        raise NotImplementedError
+        '''
+        Returns the number of bedrooms in the property
+        '''
+        return self.bedrooms
     
     def set_bathrooms(self, bathrooms: int) -> None:
-        raise NotImplementedError
+        ''''
+        Sets the number of bathrooms in the property
+        '''
+        self.bathrooms = bathrooms
     
     def get_bathrooms(self) -> int:
-        raise NotImplementedError
+        '''
+        Returns the number of bathrooms in the property
+        '''
+        return self.bathrooms
     
     def set_parking_spaces(self, parking_spaces: int) -> None:
-        raise NotImplementedError
+        '''
+        Sets the number of parking spaces in the property
+        '''
+        self.parking_spaces = parking_spaces
 
     def get_parking_spaces(self) -> int:
-        raise NotImplementedError
+        '''
+        Returns the number of parking spaces in the property
+        '''
+        return self.parking_spaces
     
     def get_coordinates(self) -> Tuple[float, float]:
-        raise NotImplementedError
+        '''
+        Returns the coordinates of the property
+        '''
+        return self.coordinates
     
     def set_floor_number(self, floor_number: int) -> None:
-        raise NotImplementedError
+        '''
+        Sets the floor number of the property
+        '''
+        self.floor_number = floor_number
 
     def get_floor_number(self) -> Union[int,None]:
-        raise NotImplementedError
+        '''
+        Returns the floor number of the property. Will return None if the property is a house
+        '''
+        return None
     
     def set_land_area(self, land_area: int) -> None:
-        raise NotImplementedError
+        '''
+        Sets the land area of the property
+        '''
+        self.land_area = land_area
 
     def get_land_area(self) -> Union[int,None]:
-        raise NotImplementedError
+        '''
+        Returns the land area of the property. Will return None if the property is an apartment
+        '''
+        return None
     
     def set_floor_area(self, floor_area: int) -> None:
-        raise NotImplementedError
+        '''
+        Sets the floor area of the property
+        '''
+        self.floor_area = floor_area
     
     def get_floor_area(self) -> int:
-        raise NotImplementedError
+        '''
+        Returns the floor area of the property
+        '''
+        return self.floor_area
 
     def set_price(self, price: int) -> None:
-        raise NotImplementedError
+        '''
+        Sets the price of the property
+        '''
+        self.price = price
     
     def get_price(self) -> int:
-        raise NotImplementedError
+        '''
+        Returns the price of the property
+        '''
+        return self.price
     
     def set_property_features(self, property_features: List[str]) -> None:
-        raise NotImplementedError
+        '''
+        Sets the property features of the property
+        '''
+        self.property_features = property_features
     
     def get_property_features(self) -> List[str]:
-        raise NotImplementedError
+        '''
+        Returns the property features of the property
+        '''
+        return self.property_features
 
 if __name__ == '__main__':
     pass
